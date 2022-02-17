dfecebe88a1f8f67d308d0b209b3cbb832d97e55
Madmurt <mccartm6@gmail.com>
2022-02-17 08:20:58 +0000

    Updated todo list

--- src/App.tsx
+++ src/App.tsx
@@ -109,14 +109,20 @@ export default App;
 /**
  * Stage 1.
  *
- * Get data from api - basic fetch request for now
- * create search input -= basic css for now
- * when user clicks on the search input showw all managers - basic item css for now
- *      - show only full name for now
- * restrict i to show just 2 at atime
- * create basic filter functionality
- *        - case insensitive
- *        - both names
+ * Get data from api - basic fetch request for now --DONE
+ * create search input -= basic css for now -- DONE
+ * when user clicks on the search input show all managers - basic item css for now -- DONE
+ *      - show only full name for now -- DONE
+ * restrict i to show just 2 at atime -- DONE
+ * create basic filter functionality -- DONE
+ *        - case insensitivE -- DONE
+ *        - both names -- dONE
+ * 
+ * Stage 2.
+ * 
+ * Enhance filtering functionality to match all requirements  --DONE
+ * Add keyboard navigation -- DONE
+ * Add focus requirements -- DONE
  *
  * Basic component structure
  *      Manager search component
@@ -125,8 +131,8 @@ export default App;
  *            --> search item component
  *
  * **** BUGS ****
- * when you navigate down with keyboard list should scroll with you 
- * disble input while loading
+ * TODO : when you navigate down with keyboard list should scroll with you 
+ * disable input while loading -- FIXED
  * 
  * 
 
@@ -135,7 +141,7 @@ export default App;
  ******Things to look into again if i have time
  * -- selecting by data-name
  * 				-- could possibly get key and then search for that key in list returning name value
- *-- can i use props in styled components without passing them to the warapper?
+ *-- can i use props in styled components without passing them to the wrapper?
  *-- 
  *
  * *****Things to consider
